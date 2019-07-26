541cdbdb105077b522782012b9cb57983d85a5b4
Greg Barry <gregory.paul.barry@gmail.com>
2019-07-26 12:23:13 -0500

    Add ability to set word, shouldPost flag, async cleanup, etc

---
+++ defs.js
@@ -0,0 +1,76 @@
+export const defs = {
+    orgulous: [{
+        'word': 'orgulous',
+        'functional_label': 'adjective',
+        'pronunciation': [
+            'http://media.merriam-webster.com/soundc11/o/orgulo01.wav'
+        ],
+        'etymology': 'Middle English, from Anglo-French [orguillus,] from [orguil] pride, of Germanic origin; akin to Old High German [urguol] distinguished',
+        'definition': [{
+            'meanings': [
+                ':'
+            ],
+            'synonyms': [
+                'proud'
+            ],
+            'senses': []
+        }]
+    }],
+    overweening: [{
+        'word': 'overweening',
+        'functional_label': 'adjective',
+        'pronunciation': [
+            'http://media.merriam-webster.com/soundc11/o/overwe04.wav'
+        ],
+        'etymology': 'Middle English [overwening,] present participle of [overwenen] to be arrogant, from [over] + [wenen] to ween',
+        'definition': [{
+            'number': '1',
+            'meanings': [
+                ': '
+            ],
+            'synonyms': [
+                'arrogant',
+                'presumptuous'
+            ]
+        }, {
+            'number': '2',
+            'meanings': [
+                ': '
+            ],
+            'synonyms': [
+                'immoderate',
+                'exaggerated'
+            ]
+        }]
+    }],
+    exigent: [{
+        'word': 'exigent',
+        'functional_label': 'adjective',
+        'pronunciation': [
+            'http://media.merriam-webster.com/soundc11/e/exigen04.wav'
+        ],
+        'etymology': 'Latin [exigent-, exigens,] present participle of [exigere] to demand',
+        'definition': [{
+            'number': '1',
+            'meanings': [
+                ': requiring immediate aid or action '
+            ],
+            'illustrations': [
+                'exigent circumstances'
+            ]
+        }, {
+            'number': '2',
+            'meanings': [
+                ': requiring or calling for much : '
+            ],
+            'synonyms': [
+                'demanding'
+            ],
+            'illustrations': [
+                'an exigent client'
+            ]
+        }],
+        'popularity': 'Top 30% of words'
+    }]
+};
+
--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 require('dotenv').config();
-import _ from 'lodash';
+import {get} from 'lodash';
 import cheerio from 'cheerio';
 import got from 'got';
 import logger from 'bristol';
 import {CollegiateDictionary} from 'mw-dict';
 import moment from 'moment';
 import snoowrap from 'snoowrap';
+import {argv} from 'yargs';
 
 const {mwKey} = process.env;
 const dictionary = new CollegiateDictionary(mwKey);
 const random = Math.floor(Math.random() * (2000 - 0 + 1)) + 0;
-let days = random;
 
-logger.addTarget('console');
-logger.info(`Using ${random} as a date seed`);
+let days = random;
+let {word, shouldPost = true} = argv;
+let hasSeedWord = !!word;
 
 const date = moment().subtract(days, 'days').format('Y/MM/DD');
 const today = moment().format('MMMM Do, Y');
 const mwUrl = 'https://www.merriam-webster.com';
 const redditBase = 'https://www.reddit.com/r/WordOfTheDay/search/.json?q=';
 const redditOptions = '&restrict_sr=on&sort=relevance&t=all';
+const configObj = {date, hasSeedWord, shouldPost, word};
+
+logger.addTarget('console');
+logger.info(`Using ${random} as a date seed`);
 
 const getWord = async url => {
     logger.info('grabbing a word of the day from merriam-webster.com');
@@ -33,59 +38,85 @@ const getRedditResults = async word => {
     logger.info('checking reddit to see if word has been posted before');
     const url = `${redditBase}${word}${redditOptions}`;
     const res = await got(url, {json: true});
-    const {children: results} = _.get(res, 'body.data', []);
+    const {children: results} = get(res, 'body.data', []);
     return results;
 };
 
 const startOver = () => {
     days++;
     const previousDay = moment().subtract(days, 'days').format('Y/MM/DD');
-    main(previousDay);
+    const configObj = {
+        date: previousDay,
+        hasSeedWord: false,
+        shouldPost,
+        word: ''
+    };
+
+    main(configObj);
 };
 
 const cleanMeanings = (meanings = []) => {
     const joinedMeanings = meanings.length > 1 ? meanings.join(' ; ') : meanings[0];
-    const cleanedMeanings = joinedMeanings.replace(/:/g, '');
-    return cleanedMeanings.trim();
+    if (joinedMeanings) {
+        const cleanedMeanings = joinedMeanings.replace(/:/g, '');
+        return cleanedMeanings.trim();
+    }
+
+    return;
 };
 
-const main = async date => {
-    const word = await getWord(`${mwUrl}/word-of-the-day/${date}`);
-    logger.info(`found ${word} for ${date}`);
+const main = async configObj => {
+    let {hasSeedWord, word} = configObj;
+    const {date, shouldPost} = configObj;
+
+    if (!word) {
+        word = await getWord(`${mwUrl}/word-of-the-day/${date}`);
+    }
+
+    logger.info(`found ${word} for ${hasSeedWord ? today : date}`);
+
+    const attribution = `${mwUrl}/dictionary/${word}`;
     const redditResults = await getRedditResults(word);
+
     if (redditResults && redditResults.length === 0) {
-        dictionary.lookup(word).then(result => {
-            const attribution = `${mwUrl}/dictionary/${word}`;
-            const [{
-                definition: definitions = [],
-                functional_label: type = '',
-                popularity = ''
-            }] = result;
+        const result = await dictionary.lookup(word);
 
-            const popularityText = popularity ? `Popularity: ${popularity}\n\n` : '';
-            let lastNumber = 1;
-            let title = `${today} - ${word}`;
-            let text = `_${type}_\n\n`;
-            let defs = '';
+        if (!result) {
+            logger.info('there was an error found within this definition object');
+            startOver();
+        }
 
-            definitions.forEach((definition, i) => {
-                const {meanings = []} = definition;
-                const cleanedMeanings = cleanMeanings(meanings);
-                if (cleanedMeanings) {
-                    if (i === 0) {
-                        title = `${title} - ${cleanedMeanings}`;
-                    }
+        const [{
+            definition: definitions = [],
+            functional_label: type = '',
+            popularity = ''
+        }] = result;
 
-                    defs = `${defs}${lastNumber}.${cleanedMeanings}\n\n`;
-                    lastNumber++;
+        const popularityText = popularity ? `Popularity: ${popularity}\n\n` : '';
+        let lastNumber = 1;
+        let title = `${today} - ${word}`;
+        let text = `_${type}_\n\n`;
+        let defs = '';
+
+        definitions.forEach((definition, i) => {
+            const {meanings = []} = definition;
+            const cleanedMeanings = cleanMeanings(meanings);
+            if (cleanedMeanings) {
+                if (i === 0) {
+                    title = `${title} - ${cleanedMeanings}`;
                 }
-            });
 
-            text = `${text}${defs}${popularityText}[merriam-webster.com](${attribution})`;
+                defs = `${defs}${lastNumber}. ${cleanedMeanings}\n\n`;
+                lastNumber++;
+            }
+        });
 
-            logger.info('title', title);
-            logger.info('text', text);
+        text = `${text}${defs}${popularityText}[merriam-webster.com](${attribution})`;
 
+        logger.info('title', title);
+        logger.info('text', text);
+
+        if (shouldPost) {
             const {clientId, clientSecret, redditUser: username, redditPass: password} = process.env;
             const r = new snoowrap({
                 userAgent: 'reddit-bot-example-node',
@@ -95,20 +126,19 @@ const main = async date => {
                 password
             });
 
-            r.getSubreddit('wordoftheday').submitSelfpost({
+            const postResult = await r.getSubreddit('wordoftheday').submitSelfpost({
                 title,
                 text
             });
 
-            logger.info('posted word of the day to reddit!');
-        }).catch(err => {
-            logger.info('there was an error found within this definition object', err);
-            startOver();
-        });
+            logger.info('posted word of the day to reddit!', postResult);
+        }
+
+        process.exit();
     } else {
         logger.info('word was already used in some capacity, go back another day');
         startOver();
     }
 };
 
-main(date);
+main(configObj);
